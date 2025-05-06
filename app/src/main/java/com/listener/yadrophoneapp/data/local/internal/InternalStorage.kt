edfc8b1da5c6320614dacaf8ecee4c32fa7944c8
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2025-05-06 18:01:00 +0400

    Implemented getting contacts only from device

--- app/src/main/java/com/listener/yadrophoneapp/data/local/internal/InternalStorage.kt
+++ app/src/main/java/com/listener/yadrophoneapp/data/local/internal/InternalStorage.kt
@@ -13,47 +13,51 @@ import javax.inject.Inject
 
 class InternalStorage @Inject constructor(private val context: Context) : LocalDataSource {
 
-    override suspend fun getAllContactsFromDevice(): Flow<Result<List<Contact>>> = ResultUtils.requestFlow {
-        val contacts = mutableListOf<Contact>()
+    override suspend fun getAllContactsFromDevice(): Flow<Result<List<Contact>>> =
+        ResultUtils.requestFlow {
+            val contacts = mutableListOf<Contact>()
 
-        val projection = arrayOf(
-            ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER,
-            ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI
-        )
+            val projection = arrayOf(
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI
+            )
 
-        val sortOrder = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} ASC"
+            val sortOrder = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} ASC"
 
-        val cursor = context.contentResolver.query(
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            projection,
-            null,
-            null,
-            sortOrder
-        )
+            val selection = "${ContactsContract.RawContacts.ACCOUNT_TYPE} = ?"
+            val selectionArgs = arrayOf("com.android.contacts.default")
 
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                val id = cursor.getString(cursor.getColumnIndexOrThrow(projection[0]))
-                val displayName = cursor.getString(cursor.getColumnIndexOrThrow(projection[1]))
-                val phoneNumber = cursor.getString(cursor.getColumnIndexOrThrow(projection[2]))
-                val photo = cursor.getString(cursor.getColumnIndexOrThrow(projection[2]))
+            val cursor = context.contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                sortOrder
+            )
 
-                contacts.add(
-                    Contact(
-                        id = id,
-                        displayName = displayName,
-                        phoneNumber = phoneNumber,
-                        photo = photo
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    val id = cursor.getString(cursor.getColumnIndexOrThrow(projection[0]))
+                    val displayName = cursor.getString(cursor.getColumnIndexOrThrow(projection[1]))
+                    val phoneNumber = cursor.getString(cursor.getColumnIndexOrThrow(projection[2]))
+                    val photo = cursor.getString(cursor.getColumnIndexOrThrow(projection[3]))
+
+                    contacts.add(
+                        Contact(
+                            id = id,
+                            displayName = displayName,
+                            phoneNumber = phoneNumber,
+                            photo = photo
+                        )
                     )
-                )
 
-            } while (cursor.moveToNext())
-        }
+                } while (cursor.moveToNext())
+            }
 
-        cursor?.close()
+            cursor?.close()
 
-        contacts.toList()
-    }.flowOn(Dispatchers.IO)
+            contacts.toList()
+        }.flowOn(Dispatchers.IO)
 }
