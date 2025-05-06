ee4f4f2765714c3ba019a11fea558df3c7ac4c5a
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2025-05-06 12:36:41 +0400

    Added call phone permission

--- app/src/main/java/com/listener/yadrophoneapp/presentation/MainActivity.kt
+++ app/src/main/java/com/listener/yadrophoneapp/presentation/MainActivity.kt
@@ -2,17 +2,12 @@ package com.listener.yadrophoneapp.presentation
 
 import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,25 +15,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.content.ContextCompat
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.accompanist.permissions.shouldShowRationale
 import com.listener.yadrophoneapp.DI
+import com.listener.yadrophoneapp.R
 import com.listener.yadrophoneapp.presentation.contacts.ContactsViewModel
 import com.listener.yadrophoneapp.presentation.contacts.layout.ContactsListScreen
 import com.listener.yadrophoneapp.presentation.ui.theme.YadrophoneappTheme
@@ -60,7 +47,8 @@ class MainActivity : ComponentActivity() {
             YadrophoneappTheme {
                 val permissionsState = rememberMultiplePermissionsState(
                     permissions = listOf(
-                        Manifest.permission.READ_CONTACTS
+                        Manifest.permission.READ_CONTACTS,
+                        Manifest.permission.CALL_PHONE
                     )
                 )
                 val lifecycleOwner = LocalLifecycleOwner.current
@@ -80,32 +68,22 @@ class MainActivity : ComponentActivity() {
                     }
                 )
 
-                permissionsState.permissions.forEach { perm ->
-                    when (perm.permission) {
-                        Manifest.permission.READ_CONTACTS -> {
-                            when {
-                                perm.status.isGranted -> {
-                                    ContactsListScreen(contactsViewModel)
-                                }
-
-                                else -> {
-                                    Column(
-                                        modifier = Modifier.fillMaxSize(),
-                                        verticalArrangement = Arrangement.Center,
-                                        horizontalAlignment = Alignment.CenterHorizontally
-                                    ) {
-                                        Text("Needed permission for reading contacts")
-                                        Button(onClick = {
-                                            Intent(
-                                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                                                Uri.fromParts("package", packageName, null)
-                                            ).also(::startActivity)
-                                        }) {
-                                            Text("Go to settings")
-                                        }
-                                    }
-                                }
-                            }
+                if (permissionsState.permissions.all { it.status.isGranted }) {
+                    ContactsListScreen(contactsViewModel)
+                } else {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = stringResource(R.string.needed_permissions))
+                        Button(onClick = {
+                            Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts("package", packageName, null)
+                            ).also(::startActivity)
+                        }) {
+                            Text(text = stringResource(R.string.go_to_settings))
                         }
                     }
                 }
